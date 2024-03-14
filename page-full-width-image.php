8a72a35c63412ef9c085a822fe2c9f93c888bd92
David Alejandro Gómez <david@acapillatech.com.ar>
2024-03-14 12:18:04 -0300

    Ajuste de layout títulos de páginas y slides

--- page-full-width-image.php
+++ page-full-width-image.php
@@ -18,28 +18,21 @@ get_header();
 
       <?php $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
       <header class="entry-header featured-full-width-img height-75 bg-dark text-light mb-5" style="background-image: url('<?php echo $thumb['0']; ?>'); background-position: center center;">
-        <div class="container entry-header h-100 d-flex align-items-end pb-3">
-          <div class="post-header-wrapper">
+        <div class="w-100 h-100 d-flex align-items-end pb-3">
+          <div class="post-header-wrapper bg-dark bg-opacity-75 p-3 w-100 mb-4">
             <h1 class="entry-title p-0 m-0"><?php echo the_title(); ?></h1>
-            <?php the_excerpt(); ?>
+            <p class="mb-0"><?php echo get_the_excerpt(); ?></p>
           </div><!-- .post-header-wrapper -->
         </div>
       </header>
 
       <div class="container pb-5">
-
         <!-- Hook to add something nice -->
         <?php bs_after_primary(); ?>
 
         <div class="entry-content">
           <?php the_content(); ?>
         </div>
-
-        <footer class="entry-footer">
-
-        </footer>
-
-        <?php comments_template(); ?>
 
       </div><!-- container -->
 
