e839588448e5703ffa43053b01345696883b6fcf
David Alejandro Gómez <david@acapillatech.com.ar>
2024-04-18 11:41:18 -0300

    Adaptaciones para update a v5.3.4 parent theme

--- functions.php
+++ functions.php
@@ -7,8 +7,8 @@ function bootscore_child_enqueue_styles()
 	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
 	// Compiled Bootstrap
-	$modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/css/lib/bootstrap.min.css'));
-	wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/css/lib/bootstrap.min.css', array('parent-style'), $modified_bootscoreChildCss);
+	$modified_bootscoreChildCss = date('YmdHi', filemtime(get_parent_theme_file_path() . '/css/main.css'));
+	wp_enqueue_style('main', get_stylesheet_directory_uri() . '/css/main.css', array('parent-style'), $modified_bootscoreChildCss);
 	wp_enqueue_style('owlcarousel-css', get_stylesheet_directory_uri() . '/css/owlcarousel/owl.carousel.min.css');
 	wp_enqueue_style('owlcarousel-theme', get_stylesheet_directory_uri() . '/css/owlcarousel/owl.theme.default.min.css');
 
